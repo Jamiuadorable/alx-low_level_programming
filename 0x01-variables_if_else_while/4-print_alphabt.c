66a32fbfa36340b2d24aa7800779d5650bad85fa
Salihu Jamiu <jamiuadorable60@gmail.com>
2023-09-11 01:38:07 -0700

    a

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	/*Loop through lowercase alphabets*/
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	letter++;
+	}
+	putchar ('\n'); /* Print a newline character at the end*/
+	return (0);
+}
